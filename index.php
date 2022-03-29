70a8c248a6319dfea2cdc9922e59c096c8f8c6ec
jarriz26 <jarrizcasin@gmail.com>
2022-03-29 23:25:32 +0800

    Crud for database
    Finalizing the storing and deleting data in database

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,10 +12,37 @@
 	<div class="heading">
 		<h2 style="font-style: 'Hervetica';">ToDo List</h2>
 	</div>
-	<form method="post" action="index.php" class="input_form">
+	<form method="post" action="insert.php" class="input_form">
 		<input type="text" name="task" class="task_input">
 		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
 	</form>
+
+	<table>
+	<thead>
+		<tr>
+			<th>N</th>
+			<th>Tasks</th>
+			<th style="width: 60px;">Action</th>
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php 
+		// Displaying all the tasks in the database
+		$db = mysqli_connect("localhost", "root", "", "todo");
+		$tasks = mysqli_query($db, "SELECT * FROM tasks");
+		$db = mysqli_connect("localhost", "root", "", "todo");
+		$i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+			<tr>
+				<td> <?php echo $i; ?> </td>
+				<td class="task"> <?php echo $row['task']; ?> </td>
+				<td class="delete"> 
+					<a href="insert.php?del_task=<?php echo $row['id'] ?>">x</a> 
+				</td>
+			</tr>
+		<?php $i++; } ?>	
+	</tbody>
+</table>
 	
 </body>
 </html>
--- insert.php
+++ insert.php
@@ -15,5 +15,13 @@
 			mysqli_query($db, $sql);
 			header('location: index.php');
 		}
-	}	
+	}
+	
+	if (isset($_GET['del_task'])) {
+		$id = $_GET['del_task'];
+	
+		mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
+		header('location: index.php');
+	}
+	
   ?>
