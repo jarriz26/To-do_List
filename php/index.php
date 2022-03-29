a2d26bfae5d8e31f6a5c4f9b584752b9f556490b
jarriz26 <jarrizcasin@gmail.com>
2022-03-29 17:33:34 +0800

    Creating index php and styles
    Creating input form for tasks

---
+++ php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>To-do List</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+        <div class = heading>
+            <h1>To do List</h1>
+
+            <form method = "post" action = "index.php" class = input_form>
+                <input type = "text" id = "task" name = "task_input" placeholder = "What are you going to do?"> 
+                <input type = "submit" value = "Submit">
+
+            </form>
+
+        
+        </div>
+    </body>
+</html>
