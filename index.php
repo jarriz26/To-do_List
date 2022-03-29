b1f5ca788b64a6c645ae52f7798ce449bd257a1f
jarriz26 <jarrizcasin@gmail.com>
2022-03-29 23:06:37 +0800

    Updating the index

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="heading">
+		<h2 style="font-style: 'Hervetica';">ToDo List</h2>
+	</div>
+	<form method="post" action="index.php" class="input_form">
+		<input type="text" name="task" class="task_input">
+		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+	</form>
+	
+</body>
+</html>
